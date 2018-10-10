81924d642d915f1eed0004f4c463272a3156312d
arslandogar <arslandogar112@yahoo.com>
2018-10-10 22:39:16 +0500

    Add persons and view persons

---
+++ PhoneBook/PhoneBook/Controllers/PersonController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneBook.Models;
+using Microsoft.AspNet.Identity;
+namespace PhoneBook.Controllers
+{
+    public class PersonController : Controller
+    {
+        PhoneBookDbEntities ph = new PhoneBookDbEntities();
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(ph.People.ToList());
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create]\
+        [HttpPost]
+        public ActionResult Create(Person p)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                p.AddedBy = User.Identity.GetUserId();
+                ph.People.Add(p);
+                ph.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PhoneBook/PhoneBook/Models/CurrentUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Principal;
+
+namespace PhoneBook.Models
+{
+    public class CurrentUser
+    {
+        public static IIdentity user_id;
+    }
+}
